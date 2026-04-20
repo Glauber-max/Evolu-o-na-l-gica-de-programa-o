c66678b9a9c489a2f3f754e14513dc88f2924e4e
Glauber Ferreira <glaubersoledade218@gmail.com>
2026-04-20 13:51:01 -0300

    media

--- Java/media.java
+++ Java/media.java
@@ -1,6 +1,17 @@
-package Java;
-public class media {
-    public static void main(String[] args) {
-        
+package Java; 
+public class media { // criar uma função que calcula a media, tentar ser o mais reutilizavel possivel
+    public static void main(String[] args) { 
+        int[] media = {3, 4};
+        float result = calcularMedia(media);
+        System.out.println(result);
+    
+    }
+    public static float calcularMedia(int[] media) {
+        int notas = 0;
+        for (int i = 0; i < media.length; i++) {
+            notas += media[i];
+        }
+        float result = (float) notas / media.length;
+        return result;
     }
 }
---
+++ python/media.py
@@ -0,0 +1,12 @@
+# vou fazer um codigo de media tentando ser o mais reutilizavel possível
+def calcular_media(list_numerica: list) -> float:
+    numeros = 0   # acumulador pra saber a soma de tudo na lista (possivel usar sum tambem)
+    qtd = len(list_numerica)    # saber a quantidade de itens na lista pra dividir m = itens / qtd itens
+    for numero in list_numerica:    # pra cada numero na lista numerica ela vai somar tudo 
+        numeros += numero
+    media = numeros / qtd # por fim ela faz o calculo da média realmente
+    return round(media, 2) 
+
+lista = [10, 9, 8, 10, 7, 5]
+medias = calcular_media(lista)
+print(medias)
