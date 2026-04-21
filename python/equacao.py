7a1557bc8939cb57eb89e42ff407547efd97f3f6
Glauber Ferreira <glaubersoledade218@gmail.com>
2026-04-21 19:25:12 -0300

    concerto da validação

--- python/equacao.py
+++ python/equacao.py
@@ -1,8 +1,8 @@
 from math import sqrt
 # vou fazer uma função utilitaria que calcula por meio de baskara o x1 e x2 de uma equação do 2 grau (ax **2 +- bx +- c = 0 )
-def doubleX(a: int, b: int, c:int): 
+def doubleX(a: float, b: float, c:float): 
     if a == 0:
-        return "a não pode ser 0", "a não pode ser zero"
+        return "a não pode ser zero", "a não pode ser zero"
     delta = (b ** 2) - (4 * a * c)
     if delta < 0:
         return "sem raizes reais", "sem raizes reais"
